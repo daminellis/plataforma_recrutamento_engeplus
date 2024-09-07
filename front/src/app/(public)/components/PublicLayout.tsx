6e9d43975da0743197880b2bb8ae9af37ea88f5c
daminellis <matheus.daminelli@gmail.com>
2024-09-06 22:53:49 -0300

    modify public layout component

--- front/src/app/(public)/components/PublicLayout.tsx
+++ front/src/app/(public)/components/PublicLayout.tsx
@@ -1,4 +1,9 @@
+"use client";
+import { useState } from "react";
+import { PrimaryButton } from "@/components/ui/button/PrimaryButton";
 import { Breadcrumb } from "@/components/ui/nav/Breadcrumb";
+import { Box, FormControl, InputLabel, MenuItem, Select, Stack, TextField } from "@mui/material";
+import { MagnifyingGlassIcon, RowsIcon } from "@radix-ui/react-icons";
 import Link from "next/link";
 
 interface PublicLayoutProps {
@@ -12,6 +17,7 @@ export const PublicLayout = ({
   showSearch,
   children,
 }: PublicLayoutProps) => {
+  const [typejob, setTypejob] = useState('');
   return (
     <>
       <section className="bg-gray-200 p-5 flex flex-col items-center">
@@ -22,10 +28,36 @@ export const PublicLayout = ({
         </header>
 
         {showSearch && (
-          <form>
-            <input type="search" placeholder="Procurar vaga" />
-            <button type="submit">Buscar</button>
-          </form>
+          <div className="bg-white shadow-md rounded-lg p-4">
+            <form className="flex gap-2 items-center">
+              <MagnifyingGlassIcon className="w-5 h-5 text-blue-500" />
+              <TextField
+                variant="standard"
+                placeholder="Cargo, palavra-chave..."
+                type="search"
+                className="flex-1"
+              />
+              <RowsIcon className="w-5 h-5 text-blue-500" />
+              <Box sx={{ minWidth: 170 }}>
+                <FormControl fullWidth>
+                  <InputLabel id="filtro">Área de atuação</InputLabel>
+                  <Select
+                    labelId="filtro"
+                    id="selecionar-filtro"
+                    value={typejob}
+                    label="Área de atuação"
+                    variant="standard"
+                    onChange={(e) => setTypejob(e.target.value)}
+                  >
+                    <MenuItem value={10}>Ten</MenuItem>
+                    <MenuItem value={20}>Twenty</MenuItem>
+                    <MenuItem value={30}>Thirty</MenuItem>
+                  </Select>
+                </FormControl>
+              </Box>
+              <PrimaryButton>Procurar</PrimaryButton>
+            </form>
+          </div>
         )}
       </section>
 
