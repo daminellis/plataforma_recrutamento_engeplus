134d3f1ab5c67577533940711ead0206c06e313c
Guichard² <jeancharles.g.guichard@gmail.com>
2024-10-05 00:27:44 -0300

    enablecors function

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -14,6 +14,7 @@ async function bootstrap() {
     new FastifyAdapter(),
   );
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
   await app.listen(3000, '0.0.0.0');
 
   if (module.hot) {
