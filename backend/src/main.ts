ac20f23ce0522c31b579ed24a083d1ef8b38603f
Guichard² <jeancharles.g.guichard@gmail.com>
2024-10-05 00:50:08 -0300

    feat: :rocket: swagger documentation

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,6 +5,7 @@ import {
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 declare const module: any;
 
@@ -13,6 +14,16 @@ async function bootstrap() {
     AppModule,
     new FastifyAdapter(),
   );
+
+  const config = new DocumentBuilder()
+  .setTitle('NestJS API Engeplus')
+  .setDescription('Documentação da API Engeplus')
+  .setVersion('1.0')
+  .addTag('nestjs')
+  .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
   await app.listen(3000, '0.0.0.0');
