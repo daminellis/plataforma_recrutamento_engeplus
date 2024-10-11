1878fe284b515ae7cab197cccca744ea976aa1d8
Guichard² <jeancharles.g.guichard@gmail.com>
2024-10-11 18:00:53 -0300

    VagaTag service and controller finished

--- backend/src/controller/vagatag.controller.ts
+++ backend/src/controller/vagatag.controller.ts
@@ -1,6 +1,36 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
+import { VagaTagService } from "../service/vagatag.service";
+import { VagaTag } from "../model/vagatag.entity";
+import { CreateVagaTagDto } from "../dto/vagatags/CreateVagaTag.dto";
+import { UpdateVagaTagDto } from "../dto/vagatags/UpdateVagaTag.dto";
 
 @Controller('vagatags')
 export class VagaTagController {
     
+    constructor(private vagaTagService: VagaTagService) {}
+
+    @Get('/all')
+    async findAll(): Promise<VagaTag[]> {
+        return this.vagaTagService.findAllVagaTags();
+    }
+
+    @Get('/find/:id')
+    async findOne(@Param('id') id: number): Promise<VagaTag | null> {
+        return this.vagaTagService.findOneVagaTag(id);
+    }
+
+    @Post('/create')
+    async createVagaTag(@Body() createVagaTagDto: CreateVagaTagDto): Promise<VagaTag> {
+        return this.vagaTagService.create(createVagaTagDto);
+    }
+
+    @Put('/update/:id')
+    async updateVagaTag(@Param('id') id: number, @Body() updateVagaTagDto: UpdateVagaTagDto): Promise<VagaTag> {
+        return this.vagaTagService.update(id, updateVagaTagDto);
+    }
+
+    @Delete('/delete/:id')
+    async deleteVagaTag(@Param('id') id: number): Promise<void> {
+        return this.vagaTagService.delete(id);
+    }
 }
--- backend/src/service/vagatag.service.ts
+++ backend/src/service/vagatag.service.ts
@@ -1,6 +1,6 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
-import { Repository } from "typeorm";
+import { DeepPartial, Repository } from "typeorm";
 import { VagaTag } from "../model/vagatag.entity";
 import { CreateVagaTagDto } from "../dto/vagatags/CreateVagaTag.dto";
 import { UpdateVagaTagDto } from "../dto/vagatags/UpdateVagaTag.dto";
@@ -18,8 +18,25 @@ export class VagaTagService {
         return this.vagatagsRepository.findOneBy({ id });
     }
 
-    // async create(createVagaTagDto: CreateVagaTagDto): Promise<VagaTag> {
-    //     const newVagaTag = this.vagatagsRepository.create(createVagaTagDto);
-    //     return this.vagatagsRepository.save(newVagaTag);
-    // }
+    async create(createVagaTagDto: CreateVagaTagDto): Promise<VagaTag> {
+        const newVagaTag = this.vagatagsRepository.create(createVagaTagDto as DeepPartial<VagaTag>);
+        return this.vagatagsRepository.save(newVagaTag);
+    }
+
+    async update(id: number, updateVagaTagDto: UpdateVagaTagDto): Promise<VagaTag> {
+        const vagaTag = await this.vagatagsRepository.findOneBy({ id });
+        if (!vagaTag) {
+            throw new NotFoundException('VagaTag não encontrada');
+        }
+        Object.assign(vagaTag, updateVagaTagDto);
+        return this.vagatagsRepository.save(vagaTag);
+    }
+
+    async delete(id: number): Promise<void> {
+        const vagaTag = await this.vagatagsRepository.findOneBy({ id });
+        if (!vagaTag) {
+            throw new NotFoundException('VagaTag não encontrada');
+        }
+        await this.vagatagsRepository.delete(id);
+    }
 }
