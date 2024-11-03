b963381f8289aa2ae83ac6eb6169b646410fcc26
Guichard² <jeancharles.g.guichard@gmail.com>
2024-11-03 16:48:38 -0300

    pega Lucas 900

--- backend/src/database/database.module.ts
+++ backend/src/database/database.module.ts
@@ -18,6 +18,14 @@ import CandidaturaTag from 'src/model/candidaturatag.entity';
 import { CandidaturaTagModule } from 'src/module/candidaturatag.module';
 import BancoTalentos from 'src/model/bancotalentos.entity';
 import { BancoTalentosModule } from 'src/module/bancotalentos.module';
+import {CreateTableVaga1730253034085} from './migration/1730253034085-create_table_vaga';
+import {CreateTableUsuario1730253082745} from './migration/1730253082745-create_table_usuario';
+import {CreateTableSetor1730253130462} from './migration/1730253130462-create_table_setor';
+import {CreateTableCargo1730253140445} from './migration/1730253140445-create_table_cargo';
+import {CreateTableTag1730253145492} from './migration/1730253145492-create_table_tag';
+import {CreateTableCandidatura1730253163974} from './migration/1730253163974-create_table_candidatura';
+import {CreateTableCandidaturatag1730253172222} from './migration/1730253172222-create_table_candidaturatag';
+import {CreateTableBancotalentos1730253191858} from './migration/1730253191858-create_table_bancotalentos';
 
 @Module({
   imports: [
@@ -34,8 +42,18 @@ import { BancoTalentosModule } from 'src/module/bancotalentos.module';
         password: configService.get<string>('DB_PASSWORD'),
         database: configService.get<string>('DB_DATABASE'),
         entities: [Candidatura, CandidaturaTag, Cargo, Setor, Tag, Usuario, Vaga, BancoTalentos],
-        synchronize: true,
-        // migrations: [],
+        migrations: 
+        [
+          CreateTableSetor1730253130462,
+          CreateTableCargo1730253140445,
+          CreateTableUsuario1730253082745,
+          CreateTableTag1730253145492,
+          CreateTableVaga1730253034085,
+          CreateTableCandidatura1730253163974,
+          CreateTableCandidaturatag1730253172222,
+          CreateTableBancotalentos1730253191858
+        ],
+        migrationsRun: true,
       }),
       inject: [ConfigService],
     }),
